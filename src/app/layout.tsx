82373e117d24772bb7c377b38036642a26ff1f56
alok-kiran <alokkiran777@gmail.com>
2024-08-07 23:20:26 +0400

    added undo, redo save export features components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head></head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Graphic master</title>
+        <link rel="icon" href="/logo.svg" />
       <body className={inter.className}>{children}</body>
     </html>
   );
