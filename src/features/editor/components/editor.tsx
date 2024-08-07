915f289c564221dfb477517a5017f0d2f4b4a182
alok-kiran <alokkiran777@gmail.com>
2024-08-07 22:23:45 +0400

    added sidebar , footer , Navbar

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -2,6 +2,10 @@
 import React, { useEffect, useRef } from 'react'
 import { fabric } from "fabric";
 import { useEditor } from '../hooks/use-editor'
+import Navbar from './navbar';
+import Toolbar from './toolbar';
+import { Sidebar } from './sidebar';
+import Footer from './footer';
 
 function Editor() {
     const { init } = useEditor();
@@ -25,9 +29,17 @@ function Editor() {
       }, [init]);
 
   return (
-    <div className=' h-full flex'>
-        <div ref={containerRef} className=' flex-1 h-full bg-muted'>
-        <canvas ref={canvasRef}></canvas>
+    <div className=' h-full flex flex-col'>
+        <Navbar />
+        <div className=' absolute h-[calc(100%-68px)] w-full top-[68px] flex'>
+            <Sidebar />
+        <main className=' bg-muted flex-1 overflow-auto relative flex flex-col'>
+            <Toolbar />
+            <div ref={containerRef} className=' flex-1 h-[calc(100%-124px)] bg-muted'>
+            <canvas ref={canvasRef}></canvas>
+        </div>
+        <Footer />
+        </main>
     </div>
     </div>
   )
