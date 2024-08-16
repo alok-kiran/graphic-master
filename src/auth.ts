b0ca53085817d5e4bf9e0ef2e05e6d1511936258
alok-kiran <alokkiran777@gmail.com>
2024-08-16 23:52:21 +0400

    added database

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,9 @@
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import NextAuth from "next-auth"
 import Github from "next-auth/providers/github"
+import { db } from './db/drizzle';
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [Github],
 })
